39669cb0a7d963c85b2aefae6860cce76e8603f0
Katarina Ivo�evi? <E315-2014@ftn.ns>
2018-06-11 13:29:10 +0200

    Dodat token u login service

--- Aplikacija/hello-world/src/app/Services/login.service.ts
+++ Aplikacija/hello-world/src/app/Services/login.service.ts
@@ -4,19 +4,63 @@ import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import { HttpHeaders } from '@angular/common/http/src/headers';
 
 @Injectable()
 export class LoginService {
 
   constructor(private http:Http) { }
 
-  postMethod(newMember):void
+  getToken()
   {
-    // promeniti putanju da radi interpreter za tokene /oauth/token
-    this.http.post("http://localhost:51680/api/services",newMember)
-    .subscribe(
-      (data) => {debugger}, // Reach here if res.status >= 200 && <= 299
-      (err) => {debugger} // Reach here if fails
-    );
+    let headers=new Headers();
+    headers.append('Content-type','application/x-www-form-urlencoded');
+
+    if(!localStorage.jwt)
+    {
+      let x=this.http.post('http://localhost:51680/oauth/token',`username=admin&password=admin&grant_type=password`,{"headers":headers}) as Observable<any>;
+      
+      x.subscribe(
+        res=>
+        { 
+          res = res.json()
+          console.log(res);
+          
+          let jwt=res.access_token;
+          debugger
+          let jwtData=jwt.split('.')[1]
+          let decodedJwtJsonData=window.atob(jwt)
+          let decodedJwtData=JSON.parse(decodedJwtJsonData)
+          let role=decodedJwtData.role
+
+          console.log('jwtData: ' + jwtData)
+          console.log('decodedJwtJsonData: ' + decodedJwtJsonData)
+          console.log('decodedJwtData: ' + decodedJwtData)
+          console.log('Role ' + role)
+          
+          localStorage.setItem('jwt',jwt)
+          localStorage.setItem('role',role);
+        },
+        err =>
+        {
+          console.log("Error occured while getting token in post part");
+        }
+      );
+    }
+    else
+    {
+      let x=this.http.get('http://localhost:51680/api/Services') as Observable<any>;
+      
+      x.subscribe(
+        res=>
+        {
+          console.log(res);
+        },
+        err=>
+        {
+          console.log("Error occured while getting token in get part");
+        }
+      );
+    }
   }
 }
